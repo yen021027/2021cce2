734f166985067dfa2f693c90cf63da8ecc71bd2d
yen021027 <91250422+yen021027@users.noreply.github.com>
2022-04-15 12:02:16 +0800

    Add files via upload

---
+++ week08-1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int N;
+    scanf"("%d",&N);
+    for(int i=0;i<N;i++){
+        scanf("%s",nation[i]);
+        char name[80];
+        gets(name);
+    }
+    for(int i=0;i<N;i++){
+        printf("%s\n",nation[i]);
+    }
+}
